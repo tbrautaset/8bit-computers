dcc8a5bc3587dbde3c06b8db48d4270e07d2f853
tbrautaset <31731232+tbrautaset@users.noreply.github.com>
2026-09-18 11:22:57 +0200

    Delete tiki100/games/*

--- tiki100/games/invader/runtime/video/screen_reset.asm
+++
@@ -1,7 +0,0 @@
-; Exact runtime 8100h-8108h
-
-SCREEN_RESET:
-        CALL CLEAR_MODE3_ROWS                                                                                   ; 8100: CD 00 A8
-        LD A,00h                                                                                     ; 8103: 3E 00
-        LD (0A507h),A                                                                                 ; 8105: 32 07 A5
-        RET                                                                                          ; 8108: C9
